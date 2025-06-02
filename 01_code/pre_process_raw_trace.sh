4d424236d996eb53355fd57c0b928d17210dd7da
Stuart Brown <s.brown@adelaide.edu.au>
2025-06-02 16:44:10 +0930

    changes to preprocessing. Use coarse res data

--- 01_code/pre_process_raw_trace.sh
+++ 01_code/pre_process_raw_trace.sh
@@ -4,38 +4,65 @@ conda activate nco_stable
 
 cd /home/dafcluster4/Desktop/TraCE_Data/
 
-ncremap -g /home/dafcluster4/Documents/GitHub/temperature_test/02_data/00_maps/sahul_0.5r.nc -G latlon=114,110#snwe=-50.0,7.0,105.0,160.0 # 0.5x0.5 Sahul grid
+ncremap -g /home/dafcluster4/Documents/GitHub/temperature_test/02_data/00_maps/sahul_0.5r.nc -G latlon=128,113#snwe=-52.5,11.5,105.0,161.50  # 0.5x0.5 Sahul grid
+ncremap -g /home/dafcluster4/Documents/GitHub/temperature_test/02_data/00_maps/sahul_coarse.nc -G latlon=17,15#snwe=-52.5,11.25,105.0,161.25 #3.75 grid
 
 # generate a map file to downscale the non temp and precip variables
-ncremap -a neareststod -V Z3 --preserve=mean -R '--rgn_dst --rnr_thr=0.0' -g /home/dafcluster4/Documents/GitHub/temperature_test/02_data/00_maps/sahul_0.5r.nc -s ./raw/monthly/others/trace.36.400BP-1990CE.cam2.h0.Z3.2160101-2204012.nc -m /home/dafcluster4/Documents/GitHub/temperature_test/02_data/00_maps/trace_to_sahul_0.5r_neareststod.nc -o /home/dafcluster4/Documents/GitHub/temperature_test/02_data/00_maps/temp_output.nc
+ncremap -a bilinear -V Z3 --preserve=mean -R '--rgn_dst --rnr_thr=0.0' -g /home/dafcluster4/Documents/GitHub/temperature_test/02_data/00_maps/sahul_coarse.nc -s ./raw/monthly/others/trace.36.400BP-1990CE.cam2.h0.Z3.2160101-2204012.nc -m /home/dafcluster4/Documents/GitHub/temperature_test/02_data/00_maps/trace_to_sahul_coarse_neareststod.nc -o /home/dafcluster4/Documents/GitHub/temperature_test/02_data/00_maps/temp_output.nc
 
 # remap TraCE21 data to 0.5 degree grid
-output_dir="/home/dafcluster4/Documents/GitHub/temperature_test/02_data/01_inputs/"
-map_location="/home/dafcluster4/Documents/GitHub/temperature_test/02_data/00_maps/trace_to_sahul_0.5r_neareststod.nc"
+output_dir="/home/dafcluster4/Documents/GitHub/temperature_test/02_data/01_inputs/single/"
+map_location="/home/dafcluster4/Documents/GitHub/temperature_test/02_data/00_maps/trace_to_sahul_coarse_neareststod.nc"
 
-for file in ./raw/monthly/others/*; do
+for file in ./raw/monthly/*/*.nc; do
     echo "$file"
-    var=$(echo $file | cut -f 7 -d ".") # get variable from filename
-    echo -e $var
-    oname="$(basename $file .nc)"
+    # Get variable from filename
+    var=$(echo "$file" | cut -f 7 -d ".")
+    echo -e "$var"
+    # Remap with ncremap
+    oname="$(basename "$file" .nc)"
     outname="${output_dir}${oname}.Sahul.nc"
-    ncremap -v $var -m $map_location -i $file -o $outname
-    infile=$outname
+    ncremap -v "$var" -m "$map_location" -i "$file" -o "$outname"
+    infile="$outname"
+    # Prepare output filename for processed file
     unset oname outname
-    oname="$(basename $infile .nc)"
+    oname="$(basename "$infile" .nc)"
     outname="${output_dir}${oname}.1900_1989CE.nc"
-    # First step is 1900-01-16, last step is 1989-12-16
+    # Conditional processing based on variable name
     if [[ "$var" == "T" || "$var" == "U" || "$var" == "V" || "$var" == "Z3" ]]; then
-        cdo -setreftime,1900-01-16,,1month -settaxis,1900-01-16,,1month -setcalendar,365_day -seltimestep,4201/5280 -sellevidx,20,26 "$infile" "$outname"
+        cdo -setreftime,1900-01-16,,1month \
+            -settaxis,1900-01-16,,1month \
+            -setcalendar,365_day \
+            -seltimestep,4201/5280 \
+            -sellevidx,20,26 \
+            "$infile" "$outname"
     elif [[ "$var" == "RELHUM" ]]; then
-        cdo --reduce_dim -setreftime,1900-01-16,,1month -settaxis,1900-01-16,,1month -setcalendar,365_day -seltimestep,4201/5280 -sellevidx,26 "$infile" "$outname"
+        cdo --reduce_dim \
+            -setreftime,1900-01-16,,1month \
+            -settaxis,1900-01-16,,1month \
+            -setcalendar,365_day \
+            -seltimestep,4201/5280 \
+            -sellevidx,26 \
+            "$infile" "$outname"
+    elif [[ "$var" == "PRECC" || "$var" == "PRECL" ]]; then
+        cdo chunit,'m/s','kg/m2/s' \
+            -mulc,1000 \
+            -setreftime,1900-01-16,,1month \
+            -settaxis,1900-01-16,,1month \
+            -setcalendar,365_day \
+            -seltimestep,4201/5280 \
+            "$infile" "$outname"
     else
-        cdo -setreftime,1900-01-16,,1month -settaxis,1900-01-16,,1month -setcalendar,365_day -seltimestep,4201/5280 "$infile" "$outname"
+        cdo -setreftime,1900-01-16,,1month \
+            -settaxis,1900-01-16,,1month \
+            -setcalendar,365_day \
+            -seltimestep,4201/5280 \
+            "$infile" "$outname"
     fi
     unset mapfile oname outname
 done
 
-cd "/home/dafcluster4/Documents/GitHub/temperature_test/02_data/01_inputs/"
+cd "/home/dafcluster4/Documents/GitHub/temperature_test/02_data/01_inputs/single/"
 
 rm -rf *.Sahul.nc
 
@@ -44,12 +71,12 @@ cd /home/dafcluster4/Desktop/TraCE_Data/
 # convert PRECC and PRECL (m/s) to volume (kg/m2/s), assuming 1000 kg/m
 # Precipitation rate (kg/m²/s) = Precipitation rate (m/s) × density
 
-cdo chunit,'m/s','kg/m2/s' -mulc,1000 -setreftime,1900-01-16,,1month -settaxis,1900-01-16,,1month -setcalendar,365_day -seltimestep,4201/5280 -sellonlatbox,105.0,161.25,-50.0,10.0 -selname,PRECC raw/monthly/pr/trace.36.400BP-1990CE.cam2.h0.PRECC.2160101-2204012.nc /home/dafcluster4/Documents/GitHub/temperature_test/02_data/01_inputs/trace.36.400BP-1990CE.cam2.h0.PRECC.2160101-2204012.Sahul.1900_1989CE.nc
+# cdo chunit,'m/s','kg/m2/s' -mulc,1000 -setreftime,1989-01-16,,1month -settaxis,1989-01-16,,1month -setcalendar,365_day -seltimestep,5269/5280 -sellonlatbox,105.0,161.25,-50.0,10.0 -selname,PRECC raw/monthly/pr/trace.36.400BP-1990CE.cam2.h0.PRECC.2160101-2204012.nc /home/dafcluster4/Documents/GitHub/temperature_test/02_data/01_inputs/single/trace.36.400BP-1990CE.cam2.h0.PRECC.2160101-2204012.Sahul.1900_1989CE.nc
 
-cdo chunit,'m/s','kg/m2/s' -mulc,1000 -setreftime,1900-01-16,,1month -settaxis,1900-01-16,,1month -setcalendar,365_day -seltimestep,4201/5280 -sellonlatbox,105.0,161.25,-50.0,10.0 -selname,PRECL raw/monthly/pr/trace.36.400BP-1990CE.cam2.h0.PRECL.2160101-2204012.nc /home/dafcluster4/Documents/GitHub/temperature_test/02_data/01_inputs/trace.36.400BP-1990CE.cam2.h0.PRECL.2160101-2204012.Sahul.1900_1989CE.nc
+# cdo chunit,'m/s','kg/m2/s' -mulc,1000 -setreftime,1989-01-16,,1month -settaxis,1989-01-16,,1month -setcalendar,365_day -seltimestep,5269/5280 -sellonlatbox,105.0,161.25,-50.0,10.0 -selname,PRECL raw/monthly/pr/trace.36.400BP-1990CE.cam2.h0.PRECL.2160101-2204012.nc /home/dafcluster4/Documents/GitHub/temperature_test/02_data/01_inputs/single/trace.36.400BP-1990CE.cam2.h0.PRECL.2160101-2204012.Sahul.1900_1989CE.nc
 
-cdo setreftime,1900-01-16,,1month -settaxis,1900-01-16,,1month -setcalendar,365_day -sellonlatbox,105.0,161.25,-50.0,10.0 -selname,TSMN -seltimestep,4201/5280 ./raw/monthly/ts/trace.36.400BP-1990CE.cam2.h0.TS.2160101-2204012.nc /home/dafcluster4/Documents/GitHub/temperature_test/02_data/01_inputs/trace.36.400BP-1990CE.cam2.h0.TS.2160101-2204012.Sahul.1900_1989CE.nc
+# cdo setreftime,1989-01-16,,1month -settaxis,1989-01-16,,1month -setcalendar,365_day -sellonlatbox,105.0,161.25,-50.0,10.0 -selname,TS -seltimestep,5269/5280 ./raw/monthly/ts/trace.36.400BP-1990CE.cam2.h0.TS.2160101-2204012.nc /home/dafcluster4/Documents/GitHub/temperature_test/02_data/01_inputs/single/trace.36.400BP-1990CE.cam2.h0.TS.2160101-2204012.Sahul.1900_1989CE.nc
 
-cdo setreftime,1900-01-16,,1month -settaxis,1900-01-16,,1month -setcalendar,365_day -sellonlatbox,105.0,161.25,-50.0,10.0 -selname,TSMN -seltimestep,4201/5280 ./raw/monthly/ts/trace.36.400BP-1990CE.cam2.h0.TSMN.2160101-2204012.nc /home/dafcluster4/Documents/GitHub/temperature_test/02_data/01_inputs/trace.36.400BP-1990CE.cam2.h0.TSMN.2160101-2204012.Sahul.1900_1989CE.nc
+# cdo setreftime,1989-01-16,,1month -settaxis,1989-01-16,,1month -setcalendar,365_day -sellonlatbox,105.0,161.25,-50.0,10.0 -selname,TSMN -seltimestep,5269/5280 ./raw/monthly/ts/trace.36.400BP-1990CE.cam2.h0.TSMN.2160101-2204012.nc /home/dafcluster4/Documents/GitHub/temperature_test/02_data/01_inputs/single/trace.36.400BP-1990CE.cam2.h0.TSMN.2160101-2204012.Sahul.1900_1989CE.nc
 
-cdo setreftime,1900-01-16,,1month -settaxis,1900-01-16,,1month -setcalendar,365_day -sellonlatbox,105.0,161.25,-50.0,10.0 -selname,TSMX -seltimestep,4201/5280 ./raw/monthly/ts/trace.36.400BP-1990CE.cam2.h0.TSMX.2160101-2204012.nc /home/dafcluster4/Documents/GitHub/temperature_test/02_data/01_inputs/trace.36.400BP-1990CE.cam2.h0.TSMX.2160101-2204012.Sahul.1900_1989CE.nc
+# cdo setreftime,1989-01-16,,1month -settaxis,1989-01-16,,1month -setcalendar,365_day -sellonlatbox,105.0,161.25,-50.0,10.0 -selname,TSMX -seltimestep,5269/5280 ./raw/monthly/ts/trace.36.400BP-1990CE.cam2.h0.TSMX.2160101-2204012.nc /home/dafcluster4/Documents/GitHub/temperature_test/02_data/01_inputs/single/trace.36.400BP-1990CE.cam2.h0.TSMX.2160101-2204012.Sahul.1900_1989CE.nc
--- 01_code/trace_elevation.r
+++ 01_code/trace_elevation.r
@@ -4,12 +4,12 @@ land <- vect(rnaturalearthhires::countries10)
 land <- crop(land, ext(106, 160, -45, 5))
 land <- aggregate(land)
 
-elev <- rast('/home/dafcluster4/Desktop/TraCE_Data/ice5g_v1.1_00.0k_1deg.nc', "orog")
-elev <- crop(rotate(elev), c(100,163,-60,10))
+elev <- rast("/home/dafcluster4/Desktop/TraCE_Data/ice5g_v1.1_00.0k_1deg.nc", "orog")
+elev <- crop(rotate(elev), c(100, 163, -60, 10))
 elev <- ifel(elev < 0, NA, elev)
 plot(elev)
 
-landmask <- rast('/home/dafcluster4/Desktop/TraCE_Data/raw/monthly/TraCE-21K.monthly.landmask.1700.1989.nc', "landsea")[[3480]]
+landmask <- rast("/home/dafcluster4/Desktop/TraCE_Data/raw/monthly/TraCE-21K.monthly.landmask.1700.1989.nc", "landsea")[[3480]]
 landmask
 landmask <- crop(rotate(landmask), ext(elev))
 landmask <- ifel(is.na(landmask), 1, NA) # invert
@@ -21,7 +21,7 @@ plot(landmask, fun = function() lines(land))
 text(land_cells)
 
 landmask[95] <- NA
-landmask[c(42, 74,75,92,93,59,232,208)] <- 1
+landmask[c(42, 74, 75, 92, 93, 59, 232, 208)] <- 1
 
 plot(landmask, fun = function() lines(land))
 text(land_cells)
@@ -42,13 +42,13 @@ elev_coarse
 plot(elev_coarse, fun = function() {
   lines(as.polygons(landmask), lwd = 1.5)
   lines(land)
-  })
+})
 
 # make elev_coarse at 0.5
-elev_coarse_fine <- project(elev_coarse, disagg(landmask_fine,2, "near"), method = "near")
-elev_coarse_fine <- crop(elev_coarse_fine, c(105,160,-50,7))
+elev_coarse_fine <- project(elev_coarse, disagg(landmask_fine, 2, "near"), method = "near")
+elev_coarse_fine <- crop(elev_coarse_fine, c(105, 160, -50, 7))
 elev_coarse_fine
-plot(elev_coarse_fine, fun = function() lines(as.polygons(disagg(landmask_fine,2, "near"))))
+plot(elev_coarse_fine, fun = function() lines(as.polygons(disagg(landmask_fine, 2, "near"))))
 
 # writeCDF
-writeCDF(elev_coarse_fine, "./02_data/01_inputs/TraCE21_elevation.nc", varname = "elevation", longname = "elevation", unit = "m", compression = 1, missval = -999, overwrite=TRUE)
+writeCDF(elev_coarse_fine, "./02_data/01_inputs/TraCE21_elevation.nc", varname = "elevation", longname = "elevation", unit = "m", compression = 1, missval = -999, overwrite = TRUE)
